b648d82ab9070c84cb54f8347454cda855c1eb61
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2018-10-23 00:56:56 +0530

    changes-in-the-user-js-file

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator'); //validate over the unique provide by mongoose;
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique:true },
